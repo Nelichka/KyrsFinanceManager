a8ff2e2a89edbc90c648d7535963e710f25cf6fa
Andreeva Nina <nandreewaa@gmail.com>
2023-02-06 20:32:21 +0300

    Initial commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public void run() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(8989);) { // стартуем сервер один(!) раз
+            while (true) { // в цикле(!) принимаем подключения
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    // обработка одного подключения
+                    out.println("server received");
+
+                    String resp = in.readLine();
+                    System.out.println(resp);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+}
